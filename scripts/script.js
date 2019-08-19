800ee9fcaf6a8aa787e60aeb6615c5ead9d7d967
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-08-19 16:47:13 -0500

    Code review: use strict

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
+//carlos-sarmiento: Aunque no hay nada más afuera de la función, es buena idea poner el use strict afuera por si se desea incluir más cosas después
+'use strict';
 function displayTimeline() {
-  'use strict';
   var imagen = document.getElementById("timeline");
   if (imagen.style.display === "none") {
     imagen.style.display = "block";
